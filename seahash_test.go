3fc9ca3a9293b8d62d10dac8998589370151ec07
Blain Smith <rebelgeek@blainsmith.com>
2017-04-26 08:06:57 -0400

    test hashing only

--- seahash_test.go
+++ seahash_test.go
@@ -1,52 +1,27 @@
-package seahash
+package seahash_test
 
 import (
 	"fmt"
 	"testing"
+
+	"github.com/blainsmith/seahash"
 )
 
 func ExampleHash() {
 	// hash some bytes
-	hash := Hash([]byte("to be or not to be"))
+	hash := seahash.Hash([]byte("to be or not to be"))
 	fmt.Println(hash)
 	// Output: 1988685042348123509
 }
 
 func TestHash(t *testing.T) {
-	if Hash([]byte("to be or not to be")) != 1988685042348123509 {
+	if seahash.Hash([]byte("to be or not to be")) != 1988685042348123509 {
 		t.Fail()
 	}
 }
 
 func BenchmarkHash(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Hash([]byte("to be or not to be"))
-	}
-}
-
-func TestDiffuse(t *testing.T) {
-	if diffuse(94203824938) != 17289265692384716055 {
-		t.Fail()
-	}
-	if diffuse(0xDEADBEEF) != 12110756357096144265 {
-		t.Fail()
-	}
-	if diffuse(0) != 0 {
-		t.Fail()
-	}
-	if diffuse(1) != 15197155197312260123 {
-		t.Fail()
-	}
-	if diffuse(2) != 1571904453004118546 {
-		t.Fail()
-	}
-	if diffuse(3) != 16467633989910088880 {
-		t.Fail()
-	}
-}
-
-func BenchmarkDiffuse(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		diffuse(94203824938)
+		seahash.Hash([]byte("to be or not to be"))
 	}
 }
