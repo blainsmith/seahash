f0b55bc6c1c45f0344e5590b4aa795374fe8ce6e
Blain Smith <rebelgeek@blainsmith.com>
2017-04-27 08:50:22 -0400

    refactor to implement hash.Hash and document

--- seahash.go
+++ seahash.go
@@ -1,46 +1,103 @@
+// Package seahash implements SeaHash, a non-cryptographic hash function
+// created by http://ticki.github.io.
+//
+// See https://ticki.github.io/blog/seahash-explained.
 package seahash
 
-import "bytes"
+import (
+	"bytes"
+	"encoding/binary"
+	"hash"
+)
 
-// Hash will hash a byte array into a uint64 with preset seeds
-func Hash(b []uint8) uint64 {
-	return HashSeeded(b, 0x16f11fe89b0d677c, 0xb480a793d8e6c86c, 0x6fe2e5aaf078ebc9, 0x14f994a4c5259381)
+// Size of a SeaHash checksum in bytes.
+const Size = 8
+
+// BlockSize of SeaHash in bytes.
+const BlockSize = 8
+
+const (
+	chunkSize = 8
+	seed1     = 0x16f11fe89b0d677c
+	seed2     = 0xb480a793d8e6c86c
+	seed3     = 0x6fe2e5aaf078ebc9
+	seed4     = 0x14f994a4c5259381
+)
+
+type digest struct {
+	state  state
+	buffer *bytes.Buffer
 }
 
-func HashSeeded(b []uint8, k1, k2, k3, k4 uint64) uint64 {
-	state := &state{k1, k2, k3, k4}
-	buffer := bytes.NewBuffer(b)
+// New creates a new SeaHash hash.Hash
+func New() hash.Hash {
+	d := &digest{}
+	d.Reset()
+	return d
+}
 
+func (d *digest) Reset() {
+	d.state.a = seed1
+	d.state.b = seed2
+	d.state.c = seed3
+	d.state.d = seed4
+	d.buffer = bytes.NewBuffer(nil)
+}
+
+// Size returns Size constant to satisfy hash.Hash interface
+func (d *digest) Size() int { return Size }
+
+// BlockSize returns BlockSize constant to satisfy hash.Hash interface
+func (d *digest) BlockSize() int { return BlockSize }
+
+func (d *digest) Write(b []byte) (nn int, err error) {
+	d.buffer.Write(b)
+	return
+}
+
+func (d *digest) Sum(b []byte) []byte {
+	d.Write(b)
+	return d.checkSum()
+}
+
+func (d *digest) checkSum() []byte {
+	bl := uint64(d.buffer.Len())
 	for {
-		if buf := buffer.Next(8); len(buf) > 0 {
-			state.write(readInt(buf))
+		if buf := d.buffer.Next(chunkSize); len(buf) > 0 {
+			d.state.update(readInt(buf))
 		} else {
 			break
 		}
 	}
 
-	return state.finish(len(b))
+	r := make([]byte, Size)
+	binary.LittleEndian.PutUint64(r, diffuse(d.state.a^d.state.b^d.state.c^d.state.d^bl))
+	return r
+}
+
+// Sum is a convenience method that returns the checksum of the byte slice
+func Sum(b []byte) []byte {
+	var d digest
+	d.Reset()
+	d.Write(b)
+	return d.checkSum()
 }
 
 type state struct {
-	A uint64
-	B uint64
-	C uint64
-	D uint64
+	a uint64
+	b uint64
+	c uint64
+	d uint64
 }
 
-func (s *state) write(x uint64) {
-	a := s.A
+func (s *state) update(x uint64) {
+	a := s.a
 	a = diffuse(a ^ x)
 
-	s.A = s.B
-	s.B = s.C
-	s.C = s.D
-	s.D = a
-}
-
-func (s *state) finish(size int) uint64 {
-	return diffuse(s.A ^ s.B ^ s.C ^ s.D ^ uint64(size))
+	s.a = s.b
+	s.b = s.c
+	s.c = s.d
+	s.d = a
 }
 
 func diffuse(x uint64) uint64 {
